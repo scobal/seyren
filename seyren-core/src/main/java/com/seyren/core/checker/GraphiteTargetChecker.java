9cbe207c595570707a0b4ea21b659c1b7b4361ca
Mark Pope <mark.1.pope@nokia.com>
2012-05-03 15:33:37 +0100

    Issue 6: Add correct fromState and improve logic around when we should raise alerts and notifications

--- seyren-core/src/main/java/com/seyren/core/checker/GraphiteTargetChecker.java
+++ seyren-core/src/main/java/com/seyren/core/checker/GraphiteTargetChecker.java
@@ -20,43 +20,77 @@ import com.seyren.core.value.GraphiteConfig;
 public class GraphiteTargetChecker implements TargetChecker {
 
 	private HttpClient client;
-    private NotificationService notificationService;
+	private NotificationService notificationService;
 	private final GraphiteConfig graphiteConfig;
-    
-    @Inject
+
+	@Inject
 	public GraphiteTargetChecker(NotificationService notificationService, GraphiteConfig graphiteConfig) {
 		client = new HttpClient(new MultiThreadedHttpConnectionManager());
-        this.notificationService = notificationService;
+		this.notificationService = notificationService;
 		this.graphiteConfig = graphiteConfig;
 	}
 
 	@Override
 	public Alert check(Check check) throws Exception {
+
 		GetMethod get = new GetMethod(String.format(graphiteConfig.getUri(), new DateTime().getMillis(), check.getTarget()));
+
 		try {
+
 			client.executeMethod(get);
 			Float value = getValue(get);
-			
-			if (isAboveErrorThreshold(check, value)) {
-                Alert alert = createAlert(check, value, AlertType.ERROR);
-                check.report(alert, notificationService);
 
-                return alert;
+			// Always create an alert
+			Alert alert = createAlert(check, value);
+
+			// Is the alert all OK
+			if (alert.getFromType() == AlertType.OK && alert.getToType() == AlertType.OK) {
+				return null;
 			}
-			if (isAboveWarnThreshold(check, value)) {
-				return createAlert(check, value, AlertType.WARN);
+
+			// Only notify if the alert has changed state
+			if (alert.getFromType() != alert.getToType()) {
+				check.report(alert, notificationService);
 			}
-			
-			return null;
+
+			return alert;
+
 		} finally {
+
 			get.releaseConnection();
 		}
+	}
+
+	private Alert createAlert(Check check, Float value) {
+		AlertType currentState = getCurrentState(check);
+
+		if (isAboveErrorThreshold(check, value)) {
+			
+			return alert(check, value, currentState, AlertType.ERROR);
+
+		} else if (isAboveWarnThreshold(check, value)) {
+			
+			return alert(check, value, currentState, AlertType.WARN);
+
+		} else {
+			
+			return alert(check, value, currentState, AlertType.OK);
+			
+		}
+	}
+
+	private AlertType getCurrentState(Check check) {
+		AlertType fromType = AlertType.OK;
+		if (check.getAlerts().size() > 0) {
+			fromType = check.getAlerts().get(check.getAlerts().size() - 1).getToType();
+		}
+		return fromType;
 	}
 
 	private Float getValue(GetMethod get) throws Exception {
 		JsonNode tree = new ObjectMapper().readTree(get.getResponseBodyAsString());
 		JsonNode points = tree.get(0).get("datapoints");
-		
+
 		// Loop through the datapoints in reverse order until we find the latest non-null value
 		for (int i = points.size() - 1; i >= 0; i--) {
 			String value = points.get(i).get(0).asText();
@@ -64,7 +98,7 @@ public class GraphiteTargetChecker implements TargetChecker {
 				return Float.valueOf(value);
 			}
 		}
-		
+
 		throw new Exception("Could not find a valid datapoint for uri: " + get);
 	}
 
@@ -76,14 +110,14 @@ public class GraphiteTargetChecker implements TargetChecker {
 		return value >= Float.valueOf(check.getError());
 	}
 
-	private Alert createAlert(Check check, Float value, AlertType type) {
+	private Alert alert(Check check, Float value, AlertType from, AlertType to) {
 		return new Alert()
-			.withValue(value.toString())
-			.withWarn(check.getWarn())
-			.withError(check.getError())
-			.withFromType(AlertType.UNKNOWN)
-			.withToType(type)
-			.withTimestamp(new DateTime());
+				.withValue(value.toString())
+				.withWarn(check.getWarn())
+				.withError(check.getError())
+				.withFromType(from)
+				.withToType(to)
+				.withTimestamp(new DateTime());
 	}
 
 }
