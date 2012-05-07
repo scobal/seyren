97c84d4b67e44ce9246b74f65863693e9c005212
Mark Pope <scobal@gmail.com>
2012-05-07 12:15:03 +0100

    Add delete to subscription ui

--- seyren-api/src/main/java/com/seyren/api/jaxrs/SubscriptionsResource.java
+++ seyren-api/src/main/java/com/seyren/api/jaxrs/SubscriptionsResource.java
@@ -1,6 +1,7 @@
 package com.seyren.api.jaxrs;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -15,5 +16,9 @@ public interface SubscriptionsResource {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	Response createSubscription(@PathParam("checkId") String checkId, Subscription subscription);
+	
+	@DELETE
+	@Path("/{subscriptionId}")
+	Response deleteSubscription(@PathParam("checkId") String checkId, @PathParam("subscriptionId") String subscriptionId);
 
 }
