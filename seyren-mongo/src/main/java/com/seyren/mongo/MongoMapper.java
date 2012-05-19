ca0aef001ce128be3bca2a06f866171e1c1627f6
Mark Pope <scobal@gmail.com>
2012-05-19 19:11:07 +0100

    Rerun license plugin with new license file

--- seyren-mongo/src/main/java/com/seyren/mongo/MongoMapper.java
+++ seyren-mongo/src/main/java/com/seyren/mongo/MongoMapper.java
@@ -1,6 +1,4 @@
 /**
- * Copyright © 2010-2011 Nokia
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
