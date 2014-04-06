6966a18e4e2fde87baf73eedd4bbf2164186ecd8
Devin Dawson <devin.dawson@gmail.com>
2014-04-06 15:23:51 -0700

    Arbitrary check search by fields and corresponding regular expression.
    Modification of checks resource parameters to add fields and regexes.
    
    Added acceptance tests demonstrating, documenting and enforcing usage of check search API.
    
    An example URL is localhost:8080/api/checks?fields=name&patterns=test&fields=description&patterns=test.*description.*

--- seyren-acceptance-tests/src/test/java/com/seyren/acceptancetests/ChecksAT.java
+++ seyren-acceptance-tests/src/test/java/com/seyren/acceptancetests/ChecksAT.java
@@ -19,9 +19,13 @@ import static com.seyren.acceptancetests.util.SeyrenDriver.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.Test;
 
 import com.github.restdriver.serverdriver.http.response.Response;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class ChecksAT {
     
@@ -93,7 +97,79 @@ public class ChecksAT {
         assertThat(put(location, body(get(location).asText(), "application/json")), hasStatusCode(200));
         deleteLocation(location);
     }
-    
+
+    @Test
+    public void testShouldGetOneCheckByIsolatedNamePattern() {
+        Set<String> locations =  createStubChecksForPatternMatching();
+
+        Response response = get(checks().withParam("fields", "name").withParam("regexes", "patternName1"));
+        assertThat(response, hasStatusCode(200));
+        JsonNode responseJson = response.asJson();
+        assertThat(responseJson, hasJsonPath("$.values", hasSize(1)));
+
+        cleanupChecks(locations);
+    }
+
+    @Test
+    public void testShouldGetAllChecksByMutualNamePattern() {
+        Set<String> locations =  createStubChecksForPatternMatching();
+
+        Response response = get(checks().withParam("fields", "name").withParam("regexes", "patternName"));
+        assertThat(response, hasStatusCode(200));
+        JsonNode responseJson = response.asJson();
+        assertThat(responseJson, hasJsonPath("$.values", hasSize(3)));
+
+        cleanupChecks(locations);
+    }
+
+    @Test
+    public void testShouldGetMatchingChecksByRegexCharacterNamePattern() {
+        Set<String> locations =  createStubChecksForPatternMatching();
+
+        Response response = get(checks().withParam("fields", "name").withParam("regexes", "patternName\\d"));
+        assertThat(response, hasStatusCode(200));
+        JsonNode responseJson = response.asJson();
+        assertThat(responseJson, hasJsonPath("$.values", hasSize(2)));
+
+        cleanupChecks(locations);
+    }
+
+    @Test
+    public void testShouldGetNoChecksByNonexistentNamePattern() {
+        Set<String> locations =  createStubChecksForPatternMatching();
+
+        Response response = get(checks().withParam("fields", "name").withParam("regexes", "doesnotexist"));
+        assertThat(response, hasStatusCode(200));
+        JsonNode responseJson = response.asJson();
+        assertThat(responseJson, hasJsonPath("$.values", hasSize(0)));
+
+        cleanupChecks(locations);
+    }
+
+    private Set<String>  createStubChecksForPatternMatching() {
+        Set<String> locations = new HashSet<String>();
+
+        locations.add(
+                createCheck("{ \"name\": \"patternName1\", \"warn\": 1.0, \"error\": 2.0 }").
+                        getHeader("Location").getValue());
+
+        locations.add(
+                createCheck("{ \"name\": \"patternName2\", \"warn\": 1.0, \"error\": 2.0 }").
+                        getHeader("Location").getValue());
+
+        locations.add(
+                createCheck("{ \"name\": \"patternNameA\", \"warn\": 1.0, \"error\": 2.0 }").
+                        getHeader("Location").getValue());
+
+        return locations;
+    }
+
+    private void cleanupChecks(Set<String> locations) {
+        for (String location : locations) {
+            deleteLocation(location);
+        }
+    }
+
     private Response createCheck(String body) {
         Response response = post(checks(), body(body, "application/json"));
         assertThat(response, hasStatusCode(201));
