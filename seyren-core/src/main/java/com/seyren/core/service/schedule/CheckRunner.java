scobal/seyren

commit 53f5f9cb679ec7b3a36511eb4b98d8ec5a31d3a8

Prevent state and last updated date update from overwriting actual changes to the rest of the check

seyren-core/src/main/java/com/seyren/core/service/schedule/CheckRunner.java
@@ -121,9 +121,7 @@ public class CheckRunner implements Runnable {
                 
             }
             
-            check.setState(worstState);
-            check.setLastCheck(DateTime.now());
-            checksStore.saveCheck(check);
+            checksStore.updateStateAndLastCheck(check.getId(), worstState, DateTime.now());
             
             if (interestingAlerts.isEmpty()) {
                 return;

seyren-core/src/main/java/com/seyren/core/store/ChecksStore.java
@@ -15,6 +15,9 @@ package com.seyren.core.store;
 
 import java.util.Set;
 
+import org.joda.time.DateTime;
+
+import com.seyren.core.domain.AlertType;
 import com.seyren.core.domain.Check;
 import com.seyren.core.domain.SeyrenResponse;
 
@@ -31,5 +34,7 @@ public interface ChecksStore {
     Check createCheck(Check check);
     
     Check saveCheck(Check check);
+
+    void updateStateAndLastCheck(String checkId, AlertType state, DateTime lastCheck);
     
 }

seyren-mongo/src/main/java/com/seyren/mongo/MongoStore.java
@@ -36,6 +36,7 @@ import com.mongodb.DBObject;
 import com.mongodb.MongoException;
 import com.mongodb.MongoURI;
 import com.seyren.core.domain.Alert;
+import com.seyren.core.domain.AlertType;
 import com.seyren.core.domain.Check;
 import com.seyren.core.domain.SeyrenResponse;
 import com.seyren.core.domain.Subscription;
@@ -162,11 +163,10 @@ public class MongoStore implements ChecksStore, AlertsStore, SubscriptionsStore 
     
     @Override
     public Check saveCheck(Check check) {
-        DBObject findObject = forId(check.getId());
-        
         DateTime lastCheck = check.getLastCheck();
         
-        DBObject updateObject = object("name", check.getName())
+        DBObject saveObject = forId(check.getId())
+                .with("name", check.getName())
                 .with("description", check.getDescription())
                 .with("target", check.getTarget())
                 .with("warn", check.getWarn().toPlainString())
@@ -176,11 +176,21 @@ public class MongoStore implements ChecksStore, AlertsStore, SubscriptionsStore 
                 .with("lastCheck", lastCheck == null ? null : new Date(lastCheck.getMillis()))
                 .with("state", check.getState().toString());
         
-        DBObject setObject = object("$set", updateObject);
-        
-        getChecksCollection().update(findObject, setObject);
+        getChecksCollection().save(saveObject);
         
         return check;
+    }
+    
+    @Override
+    public void updateStateAndLastCheck(String checkId, AlertType state, DateTime lastCheck) {
+        DBObject findObject = forId(checkId);
+        
+        DBObject partialObject = object("lastCheck", new Date(lastCheck.getMillis()))
+                .with("state", state.toString());
+        
+        DBObject setObject = object("$set", partialObject);
+        
+        getChecksCollection().update(findObject, setObject);
     }
     
     @Override

commit 1d3faa7cf0ea9883a9af4c9275215f61b9b710b2

cherry-pick 9d32621
regressions introduced by 53f5f9c
* 3c5b61a fix only wrong state

This commit fixes state *and* lastCheck - updateStateAndLastCheck hits
mongo to read check

seyren-core/src/main/java/com/seyren/core/service/schedule/CheckRunner.java
@@ -120,15 +120,14 @@ public class CheckRunner implements Runnable {
                 interestingAlerts.add(alert);
                 
             }
-            
-            checksStore.updateStateAndLastCheck(check.getId(), worstState, DateTime.now());
-            check.setState(worstState);
-            
+
+            Check updatedCheck = checksStore.updateStateAndLastCheck(check.getId(), worstState, DateTime.now());
+
             if (interestingAlerts.isEmpty()) {
                 return;
             }
             
-            for (Subscription subscription : check.getSubscriptions()) {
+            for (Subscription subscription : updatedCheck.getSubscriptions()) {
                 if (!subscription.shouldNotify(now, worstState)) {
                     continue;
                 }
@@ -136,7 +135,7 @@ public class CheckRunner implements Runnable {
                 for (NotificationService notificationService : notificationServices) {
                     if (notificationService.canHandle(subscription.getType())) {
                         try {
-                            notificationService.sendNotification(check, subscription, interestingAlerts);
+                            notificationService.sendNotification(updatedCheck, subscription, interestingAlerts);
                         } catch (Exception e) {
                             LOGGER.warn("Notifying {} by {} failed.", subscription.getTarget(), subscription.getType(), e);
                         }

seyren-core/src/main/java/com/seyren/core/store/ChecksStore.java
@@ -48,6 +48,6 @@ public interface ChecksStore {
     
     Check saveCheck(Check check);
 
-    void updateStateAndLastCheck(String checkId, AlertType state, DateTime lastCheck);
+    Check updateStateAndLastCheck(String checkId, AlertType state, DateTime lastCheck);
     
 }

seyren-mongo/src/main/java/com/seyren/mongo/MongoStore.java
@@ -254,7 +254,7 @@ public class MongoStore implements ChecksStore, AlertsStore, SubscriptionsStore 
     }
     
     @Override
-    public void updateStateAndLastCheck(String checkId, AlertType state, DateTime lastCheck) {
+    public Check updateStateAndLastCheck(String checkId, AlertType state, DateTime lastCheck) {
         DBObject findObject = forId(checkId);
         
         DBObject partialObject = object("lastCheck", new Date(lastCheck.getMillis()))
@@ -263,6 +263,8 @@ public class MongoStore implements ChecksStore, AlertsStore, SubscriptionsStore 
         DBObject setObject = object("$set", partialObject);
         
         getChecksCollection().update(findObject, setObject);
+
+        return getCheck(checkId);
     }
     
     @Override