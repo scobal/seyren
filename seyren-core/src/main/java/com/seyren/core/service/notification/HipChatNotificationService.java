2faade9df236d22846e241f080e3346e1eb04d85
Robin Osborne <robin@mailcloud.com>
2015-04-27 16:49:13 +0100

    typos and assumption on how java does string concat fixed

--- seyren-core/src/main/java/com/seyren/core/service/notification/HipChatNotificationService.java
+++ seyren-core/src/main/java/com/seyren/core/service/notification/HipChatNotificationService.java
@@ -86,14 +86,14 @@ public class HipChatNotificationService implements NotificationService {
         for (String roomId : roomIds) {
             LOGGER.info("Posting: {} to {}: {} {}", from, roomId, message, color);
             HttpClient client = HttpClientBuilder.create().build();
-            String url = baseUrl + ("/v2/room/{}/notification?auth_token={}", roomId, authToken);
+            String url = baseUrl + "/v2/room/" + roomId + "/notification?auth_token=" + authToken;
             HttpPost post = new HttpPost(url);
             
             try {
                 List<BasicNameValuePair> parameters = new ArrayList<BasicNameValuePair>();
                 parameters.add(new BasicNameValuePair("message", message));
                 parameters.add(new BasicNameValuePair("color", color.name().toLowerCase()));
-                parameters.add(new BasicNameValuePair("message_format", "html");
+                parameters.add(new BasicNameValuePair("message_format", "html"));
                 if (notify) {
                     parameters.add(new BasicNameValuePair("notify", "1"));
                 }
