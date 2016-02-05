48f6f5035c7078d8cb9486effefad5255fa3fd5d
Jiří Zůna <jiri@zunovi.cz>
2016-02-05 15:27:30 +0100

    FIX upgrade mongodb-java-driver to 3.2.x
    Also make required changes so that seyren would compile with this new
    driver.

--- seyren-mongo/src/main/java/com/seyren/mongo/MongoStore.java
+++ seyren-mongo/src/main/java/com/seyren/mongo/MongoStore.java
@@ -33,7 +33,7 @@ import org.slf4j.LoggerFactory;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.Bytes;
-import com.mongodb.CommandFailureException;
+import com.mongodb.MongoCommandException;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
@@ -103,9 +103,9 @@ public class MongoStore implements ChecksStore, AlertsStore, SubscriptionsStore 
         LOGGER.info("Dropping old indices");
         try {
             getAlertsCollection().dropIndex(new BasicDBObject("checkId", 1).append("target", 1));
-        } catch (CommandFailureException e) {
-            if (e.getCode() != -5) {
-                // -5 is the code which appears when the index doesn't exist (which we're happy with, anything else is bad news) 
+        } catch (MongoCommandException e) {
+            if (e.getCode() != 27) {
+                // 27 is the code which appears when the index doesn't exist (which we're happy with, anything else is bad news)
                 throw e;
             }
         }
