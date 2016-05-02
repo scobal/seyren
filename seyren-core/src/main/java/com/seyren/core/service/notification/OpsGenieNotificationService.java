c03d23cbc2fd7480dbaf242ec06d6bcf75c2d1f7
DesmodnPeace <d.vrede@gmail.com>
2016-05-02 09:34:49 +0200

    remove white space

--- seyren-core/src/main/java/com/seyren/core/service/notification/OpsGenieNotificationService.java
+++ seyren-core/src/main/java/com/seyren/core/service/notification/OpsGenieNotificationService.java
@@ -1,60 +1,60 @@
 /**
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
- package com.seyren.core.service.notification;
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.seyren.core.service.notification;
 
- import java.io.IOException;
- import java.text.ParseException;
- import java.util.List;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.List;
 
- import javax.inject.Inject;
- import javax.inject.Named;
+import javax.inject.Inject;
+import javax.inject.Named;
 
- import org.slf4j.Logger;
- import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
- import com.ifountain.opsgenie.client.OpsGenieClient;
- import com.ifountain.opsgenie.client.OpsGenieClientException;
- import com.ifountain.opsgenie.client.model.alert.CloseAlertRequest;
- import com.ifountain.opsgenie.client.model.alert.CloseAlertResponse;
- import com.ifountain.opsgenie.client.model.alert.CreateAlertRequest;
- import com.ifountain.opsgenie.client.model.alert.CreateAlertResponse;
- import com.ifountain.opsgenie.client.model.alert.ListAlertsRequest;
- import com.ifountain.opsgenie.client.model.alert.ListAlertsResponse;
- import com.seyren.core.domain.Alert;
- import com.seyren.core.domain.AlertType;
- import com.seyren.core.domain.Check;
- import com.seyren.core.domain.Subscription;
- import com.seyren.core.domain.SubscriptionType;
- import com.seyren.core.exception.NotificationFailedException;
- import com.seyren.core.util.config.SeyrenConfig;
+import com.ifountain.opsgenie.client.OpsGenieClient;
+import com.ifountain.opsgenie.client.OpsGenieClientException;
+import com.ifountain.opsgenie.client.model.alert.CloseAlertRequest;
+import com.ifountain.opsgenie.client.model.alert.CloseAlertResponse;
+import com.ifountain.opsgenie.client.model.alert.CreateAlertRequest;
+import com.ifountain.opsgenie.client.model.alert.CreateAlertResponse;
+import com.ifountain.opsgenie.client.model.alert.ListAlertsRequest;
+import com.ifountain.opsgenie.client.model.alert.ListAlertsResponse;
+import com.seyren.core.domain.Alert;
+import com.seyren.core.domain.AlertType;
+import com.seyren.core.domain.Check;
+import com.seyren.core.domain.Subscription;
+import com.seyren.core.domain.SubscriptionType;
+import com.seyren.core.exception.NotificationFailedException;
+import com.seyren.core.util.config.SeyrenConfig;
 
- @Named
- public class OpsGenieNotificationService implements NotificationService {
-     private static final Logger LOGGER = LoggerFactory.getLogger(OpsGenieNotificationService.class);
- 	private SeyrenConfig config;
+@Named
+public class OpsGenieNotificationService implements NotificationService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OpsGenieNotificationService.class);
+	private SeyrenConfig config;
 
-     @Inject
-     public OpsGenieNotificationService(SeyrenConfig config) {
-     	this.config = config;
-     }
+    @Inject
+    public OpsGenieNotificationService(SeyrenConfig config) {
+        this.config = config;
+    }
     
-     @Override
-     public void sendNotification(Check check, Subscription subscription, List<Alert> alerts) throws NotificationFailedException {
+    @Override
+    public void sendNotification(Check check, Subscription subscription, List<Alert> alerts) throws NotificationFailedException {
      	OpsGenieClient client = new OpsGenieClient();
 
-         try {
-             String apiKey = subscription.getTarget();
+        try {
+            String apiKey = subscription.getTarget();
  			if (check.getState() == AlertType.ERROR || check.getState() == AlertType.WARN) {
  		    	CreateAlertRequest request = new CreateAlertRequest();
  		    	request.setApiKey(apiKey);
@@ -64,7 +64,7 @@
 		
  		    	CreateAlertResponse response = client.alert().createAlert(request);
  		    	assert response.isSuccess();
-             } else if (check.getState() == AlertType.OK) {
+            } else if (check.getState() == AlertType.OK) {
              	com.ifountain.opsgenie.client.model.beans.Alert alert = findAlert(client, apiKey, getMessage(check));
             
              	CloseAlertRequest request = new CloseAlertRequest();
@@ -72,7 +72,7 @@
              	request.setApiKey(apiKey);
              	CloseAlertResponse response = client.alert().closeAlert(request);
              	assert response.isSuccess();
-             }
+            }
  		} catch (OpsGenieClientException e) {
  			LOGGER.warn(
  					"Did not send notification to OpsGenie for check in state: {}",
@@ -87,7 +87,7 @@
  					check.getState(), e);
 
  		}
-     }
+    }
 
  	private com.ifountain.opsgenie.client.model.beans.Alert findAlert(OpsGenieClient client, String apiKey, String message) throws OpsGenieClientException, IOException, ParseException {
  		ListAlertsRequest request = new ListAlertsRequest();
@@ -107,9 +107,9 @@
  		return "Check '" + check.getName() + "' has exceeded its threshold.";
  	}
 
-     @Override
-     public boolean canHandle(SubscriptionType subscriptionType) {
-         return subscriptionType == SubscriptionType.OPSGENIE;
-     }
+    @Override
+    public boolean canHandle(SubscriptionType subscriptionType) {
+        return subscriptionType == SubscriptionType.OPSGENIE;
+    }
 
- }
+}
